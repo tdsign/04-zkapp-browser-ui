a92106999f2e5c73261b41d116956c7aade8ea55
teguhsanjaya <teguhsanjaya624@gmail.com>
2022-12-13 12:43:44 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-15afcad555221cd4.js
+++ _next/static/chunks/webpack-15afcad555221cd4.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "d5a6e10cb7950d65" + ".css";
+/******/ 			return "static/css/" + "77732365dec8f8ab" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
